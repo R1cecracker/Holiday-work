20a144f70fbfa5d96097c167596a002b949cf628
R1cecracker <troy.harcoan0@gmail.com>
2024-12-04 12:06:41 +1100

    AHHHH

--- ex2.py
+++ ex2.py
@@ -1,4 +1,3 @@
-def count_characters():
-    user_input = input("Enter some text: ")
-    character_count = len(user_input)
-print(f"'{user_input}' has {character_count} characters")
+user_input = input("What is the input string? ")
+character_count = len(user_input)
+print(f"{user_input} has {character_count} characters")
