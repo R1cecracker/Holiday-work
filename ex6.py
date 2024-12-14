cf556dd385cbb9a26df74946b78a68bcf697795e
Troy Harcoan <troy.harcoan0@gmail.com>
2024-12-14 12:50:57 +1100

    ex6 complete

---
+++ ex6.py
@@ -0,0 +1,5 @@
+current = int(input("What is your current age? "))
+retire = int(input("At what age would you like to retire? "))
+
+print(f"You have {retire - current} years until you retire.")
+print(f"It's 2025 so you can retire in {retire - current + 2025}.")
