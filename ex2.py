c27c4be1bcb72e715c3a9315ec4567534121e84b
Troy Harcoan <troy.harcoan0@gmail.com>
2024-12-14 12:08:53 +1100

    ex3 complete

---
+++ ex2.py
@@ -0,0 +1,3 @@
+user_input = input("What is the input string? ")
+character_count = len(user_input)
+print(f"{user_input} has {character_count} characters")
---
+++ ex3.py
@@ -0,0 +1,4 @@
+quote = input("What's the quote? ")
+person = input("Who said the quote? ")
+
+print(f'{person} says, "{quote}".')
