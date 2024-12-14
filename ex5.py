4d5fa00de923c609dfae62b1d763753418693916
Troy Harcoan <troy.harcoan0@gmail.com>
2024-12-14 12:22:04 +1100

    ex5 complete

---
+++ ex5.py
@@ -0,0 +1,7 @@
+x = int(input("What is the first number? "))
+y = int(input("What is the second number? "))
+
+print(f"{x} + {y} =", x + y)
+print(f"{x} - {y} =", x - y)
+print(f"{x} * {y} =", x * y)
+print(f"{x} / {y} =", x // y)
